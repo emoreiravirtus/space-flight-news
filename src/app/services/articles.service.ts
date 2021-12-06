3a263b40ffe9d6aa7abb9943f2d0d7c746d66369
Elias Moreira <emoreiravirtus@gmail.com>
2021-12-06 12:47:44 -0300

    Adds Article Model

--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { httpOptions } from '../shared/network/http-options';
+import { Article } from '../shared/models/articles.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +12,18 @@ export class ArticlesService {
 
   constructor(private http: HttpClient) { }
 
-  getArticles(lastIndex?: number) {
+  getArticles(lastIndex?: number): Observable<Array<Article>> {
 
     if(lastIndex) {
-      return this.http.get(`${environment.apiURL}/articles?_limit=10&_start=${lastIndex}`, httpOptions);
+      return this.http.get<Array<Article>>(`${environment.apiURL}/articles?_limit=10&_start=${lastIndex}`, httpOptions);
     }
     else {
-      return this.http.get(`${environment.apiURL}/articles?_limit=10`, httpOptions);
+      return this.http.get<Array<Article>>(`${environment.apiURL}/articles?_limit=10`, httpOptions);
     }
     
   }
 
-  getArticlesByTerm(term: string) {
-    return this.http.get(`${environment.apiURL}/articles?title_contains=${term}`, httpOptions);
+  getArticlesByTerm(term: string): Observable<Array<Article>> {
+    return this.http.get<Array<Article>>(`${environment.apiURL}/articles?title_contains=${term}`, httpOptions);
   }
 }
